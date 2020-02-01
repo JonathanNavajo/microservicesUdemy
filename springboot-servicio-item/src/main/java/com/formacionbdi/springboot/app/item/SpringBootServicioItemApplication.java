506f7c15e68f04436287a9c4a86a0d96a36bf37f
Jonathan <j.navajo@alumnos.upm.es>
2020-02-01 12:35:09 +0100

    feat: added microservices in eureka server

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringBootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringBootServicioItemApplication.java
@@ -2,10 +2,11 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name="servicio-productos")
+// No es obligatorio anotar con eureka cliente, porque ya se configura solo
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class SpringBootServicioItemApplication {
